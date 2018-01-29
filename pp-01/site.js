41e4bfcfd7bda47d43741789f17ba6c894623255
John Paul Aldana <aldana.jpg@gmail.com>
2018-01-28 21:01:28 -0600

    Add variables and basic if logic in site.js

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,3 +1,17 @@
 $("#contact-form").on("submit", function(e) {
+  var textEmail = $("#form-email").val();
+  var textTel = $("#form-tel").val();
+  var textDate = $("#form-date").val();
+  var reEmail = /.*@.*/;
+  var reTel = /\d{10}/;
+  var formValid = true;
+  
+  if (!reEmail.test(textEmail)) {
+    formValid = false;
 
+  }
+  if (!reTel.test(textTel)) {
+    formValid = false;
+    
+  }
 });

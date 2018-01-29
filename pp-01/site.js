821f1fdbd95e131cd4a11c2aa968c87f9c0a5027
John Paul Aldana <aldana.jpg@gmail.com>
2018-01-28 21:36:05 -0600

    Add basic minAge (18 years old) check

--- pp-01/site.js
+++ pp-01/site.js
@@ -5,6 +5,7 @@ $("#contact-form").on("submit", function(e) {
   var reEmail = /.*@.*/;
   var reTel = /\d{10}/;
   var formValid = true;
+  var minAge = 18;
   $(".user-alert").remove();
 
   if (!reEmail.test(textEmail)) {
@@ -18,6 +19,40 @@ $("#contact-form").on("submit", function(e) {
   if (textDate.length == 0) {
     formValid = false;
     $("#contact-form ol").append("<li class='user-alert'>Invalid date of birth.</li>");
+  }
+  else {
+    // YYYY-MM-DD
+    var splitDate = textDate.split("-");
+    // check if Date is supported
+    var date = new Date();
+    if (typeof date == "object" && typeof date.getFullYear == "function") {
+      var currentYear = date.getFullYear();
+      var textDateYear = splitDate[0];
+      var ageValid = true;
+      if (currentYear - textDateYear < minAge) {
+        ageValid = false;
+      }
+      else if (currentYear - textDateYear == minAge) {
+        var currentMonth = date.getMonth();
+        /*
+          subtract one because Date.getMonth
+          returns 0-index months (jan=0)
+        */
+        var textDateMonth = parseInt(splitDate[1], 10) - 1;
+        var currentDay = date.getDate();
+        var textDateDay = parseInt(splitDate[2], 10);
+        if (currentMonth < textDateMonth) {
+          ageValid = false;
+        }
+        else if (currentMonth == textDateMonth && currentDay < textDateDay) {
+          ageValid = false;
+        }
+      }
+      if (!ageValid) {
+        formValid = false;
+        $("#contact-form ol").append("<li class='user-alert'>You must be at least " + minAge + " years or older.</li>");
+      }
+    }
   }
   if (formValid) {
     $(this).remove();
